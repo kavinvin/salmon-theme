6e041e15278b5cb6f0a76ab9451d353eadee9799
kavinvin <kavinvin.vin@gmail.com>
2018-11-05 00:09:52 +0700

    Add virtual env indicator

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -10,12 +10,19 @@ function fish_prompt
   set -l dirty    "⨯"
   set -l none     ""
 
+  set -l blue 66B3FF
+  set -l red FF8080
+  set -l pink FF9999
+  set -l yellow FFE666
+
   set -l normal_color     (set_color normal)
   set -l success_color    (set_color cyan)
   set -l error_color      (set_color red)
-  set -l username_color   (set_color red)
-  set -l directory_color  (set_color brown)
-  set -l repository_color (set_color green)
+  set -l username_color   (set_color $red)
+  set -l directory_color  (set_color $yellow)
+  set -l repository_color (set_color $blue)
+
+  set -Ux LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
 
   function username --no-scope-shadowing
     echo -n -s $username_color $USER $normal_color
@@ -25,7 +32,7 @@ function fish_prompt
     echo -n -s $directory_color $cwd $normal_color
   end
 
-  function git_prompt --no-scope-shadowing
+  function git_repository --no-scope-shadowing
     echo -n -s $repository_color (git_branch_name) $normal_color
   end
 
@@ -37,6 +44,12 @@ function fish_prompt
     end
   end
 
+  function git_prompt --no-scope-shadowing
+    if git_is_repo
+      echo -n on (git_repository) (git_status) 
+    end
+  end
+
   function fish_status --no-scope-shadowing
     if test $last_command_status -eq 0
       echo -n -s $success_color $fish $normal_color
@@ -45,10 +58,12 @@ function fish_prompt
     end
   end
 
-  if git_is_repo
-    echo -n (username) in (directory) on (git_prompt) (git_status) (fish_status) ""
-  else
-    echo -n (username) in (directory) (fish_status) ""
+  function venv_status --no-scope-shadowing
+    if test $VIRTUAL_ENV
+      echo -n -s "("(basename $VIRTUAL_ENV)")"
+    end
   end
+
+  echo -n (venv_status) (username) in (directory) (git_prompt) (fish_status) ""
 
 end
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,5 +1,21 @@
 function fish_right_prompt
-  set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
-  date "+%H:%M:%S"
-  set_color normal
+
+  if test "$fish_key_bindings" = "fish_vi_key_bindings"
+    switch $fish_bind_mode
+      case default
+        set_color --bold blue
+        echo N
+      case insert
+        set_color --bold green
+        echo I
+      case replace-one
+        set_color --bold red
+        echo R
+      case visual
+        set_color --bold brmagenta
+        echo V
+    end
+    set_color normal
+  end
+
 end
